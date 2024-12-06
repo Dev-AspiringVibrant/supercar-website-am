6d139efced54f69762e58b91a2d46ff0d5dcee8b
dc2Matthew <matthew.vargas24@student.cart.org>
2024-12-06 10:02:30 -0800

    1

---
+++ buying page/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+
+}
